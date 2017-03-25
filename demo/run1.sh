a14121174f52b24c9cfe18e85c113cafada578de
ruescasd <fastness@gmail.com>
2017-03-25 20:26:30 +0100

    noCompress switch in demo scripts

--- demo/run1.sh
+++ demo/run1.sh
@@ -6,6 +6,6 @@ CLASSPATH=../target/scala-2.12/nMix-assembly-0.2-SNAPSHOT.jar:../target/scala-2.
 MAINCLASS=org.nvotes.trustee.Continuous
 
 OPTIONS="-Dconfig.file=application.conf"
-OPTIMIZATIONS="-Dlibmix.gmp=true -Dlibmix.extractor=true -Dlibmix.parallel-generators=true"
+OPTIMIZATIONS="-Dlibmix.gmp=true -Dlibmix.extractor=true -Dlibmix.parallel-generators=true -Dnmix.git.disable-compression=true"
 
-java -Xmx2G -Xms2G $OPTIONS $OPTIMIZATIONS -classpath $CLASSPATH $MAINCLASS $*
+java -Xmx4G -Xms4G $OPTIONS $OPTIMIZATIONS -classpath $CLASSPATH $MAINCLASS $*
