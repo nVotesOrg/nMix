eefcaf4f8347d72ca1b392ca75be6e730641691a
ruescasd <fastness@gmail.com>
2017-03-25 08:23:28 +0100

    Indentation

--- src/main/scala/IO.scala
+++ src/main/scala/IO.scala
@@ -15,67 +15,67 @@ import sun.misc.IOUtils
  */
 object IO {
 
-	/** Returns the contents of the given file as a String, assumes UTF-8 */
-	def asString(path: Path): String = {
-		Source.fromFile(path.toFile)(StandardCharsets.UTF_8).mkString
-	}
+  /** Returns the contents of the given file as a String, assumes UTF-8 */
+  def asString(path: Path): String = {
+    Source.fromFile(path.toFile)(StandardCharsets.UTF_8).mkString
+  }
 
-	/** Returns the contents of the given file as a String, assumes UTF-8 */
-	def asString(input: InputStream): String = {
-		val ret = Source.fromInputStream(input)(scala.io.Codec.UTF8).mkString
-		input.close
-		ret
-	}
+  /** Returns the contents of the given file as a String, assumes UTF-8 */
+  def asString(input: InputStream): String = {
+    val ret = Source.fromInputStream(input)(scala.io.Codec.UTF8).mkString
+    input.close
+    ret
+  }
 
-	/** Returns the contents of the given file a a byte array
+  /** Returns the contents of the given file a a byte array
    *
    *  FIXME Uses unsupported sun.misc.IOUtils, replace when java9 is available:
    *  http://download.java.net/java/jdk9/docs/api/java/io/InputStream.html#readAllBytes--
    */
-	def asBytes(input: InputStream): Array[Byte] = {
-		val ret = IOUtils.readFully(input, -1, true)
- 		input.close
- 		ret
-	}
+  def asBytes(input: InputStream): Array[Byte] = {
+    val ret = IOUtils.readFully(input, -1, true)
+    input.close
+    ret
+  }
 
-	/** Returns the contents of the given file a a byte array
+  /** Returns the contents of the given file a a byte array
    *
    *  Alternative implementation to above
    */
-	def asBytes2(input: InputStream): Array[Byte] = {
-		val buffer = new java.io.ByteArrayOutputStream()
+  def asBytes2(input: InputStream): Array[Byte] = {
+    val buffer = new java.io.ByteArrayOutputStream()
 
-		var nRead: Int = 0
-		val data = new Array[Byte](16384)
+    var nRead: Int = 0
+    val data = new Array[Byte](16384)
 
-		while(true) {
-			nRead = input.read(data, 0, data.length)
-			if(nRead == -1)	scala.util.control.Breaks.break
-			buffer.write(data, 0, nRead)
-		}
+    while(true) {
+      nRead = input.read(data, 0, data.length)
+      if(nRead == -1) scala.util.control.Breaks.break
+      buffer.write(data, 0, nRead)
+    }
 
-		buffer.flush()
-		buffer.toByteArray()
-	}
+    buffer.flush()
+    buffer.toByteArray()
+  }
 
-	/** Writes the given content String to file, in UTF-8 */
-	def write(path: Path, content: String): Path = {
-		write(path, content.getBytes(StandardCharsets.UTF_8))
-	}
+  /** Writes the given content String to file, in UTF-8 */
+  def write(path: Path, content: String): Path = {
+    write(path, content.getBytes(StandardCharsets.UTF_8))
+  }
 
-	/** Writes the given content byte array to file */
-	def write(path: Path, content: Array[Byte]): Path = {
-		Files.write(path, content)
-	}
+  /** Writes the given content byte array to file */
+  def write(path: Path, content: Array[Byte]): Path = {
+    Files.write(path, content)
+  }
 
-	/** Writes the given content String to a temp file, in UTF-8 */
-	def writeTemp(content: String): Path = {
-		writeTemp(content.getBytes(StandardCharsets.UTF_8))
-	}
+  /** Writes the given content String to a temp file, in UTF-8 */
+  def writeTemp(content: String): Path = {
+    writeTemp(content.getBytes(StandardCharsets.UTF_8))
+  }
 
-	/** Writes the given content byte array to a temp file */
-	def writeTemp(content: Array[Byte]): Path = {
-		val tmp = Files.createTempFile("trustee", ".tmp")
-		Files.write(tmp, content)
-	}
+  /** Writes the given content byte array to a temp file */
+  def writeTemp(content: Array[Byte]): Path = {
+    val tmp = Files.createTempFile("trustee", ".tmp")
+    Files.write(tmp, content)
+  }
 }
--- src/main/scala/Statement.scala
+++ src/main/scala/Statement.scala
@@ -8,7 +8,7 @@ import org.nvotes.mix._
 /** A Statement represents some assertion an authority makes when signing
  *  the Statement's data.
  *
- *	Signatures corresponding to statements allow verifying the correct
+ *  Signatures corresponding to statements allow verifying the correct
  *  execution of the protocol. These checks must be carried out by the
  *  protocol's Actions.
  *
@@ -16,15 +16,15 @@ import org.nvotes.mix._
  *
  */
 sealed trait Statement {
-	/** Returns the RSA signature of this statement */
-	def sign(privateKey: RSAPrivateKey): Array[Byte] = {
-		Crypto.sign(this.asJson.noSpaces, privateKey)
-	}
+  /** Returns the RSA signature of this statement */
+  def sign(privateKey: RSAPrivateKey): Array[Byte] = {
+    Crypto.sign(this.asJson.noSpaces, privateKey)
+  }
 
-	/** Returns true if the the RSA signature of this statement is correct */
-	def verify(signature: Array[Byte], publicKey: RSAPublicKey): Boolean = {
-		Crypto.verify(this.asJson.noSpaces, signature, publicKey)
-	}
+  /** Returns true if the the RSA signature of this statement is correct */
+  def verify(signature: Array[Byte], publicKey: RSAPublicKey): Boolean = {
+    Crypto.verify(this.asJson.noSpaces, signature, publicKey)
+  }
 }
 
 /** Produced by bootstrap. Each authority signs the configuration. Cardinality = (1, n)
@@ -34,7 +34,7 @@ case class ConfigStatement(configHash: String) extends Statement
 
 /** Produce by each authority. Each authority signs their share. Cardinality = (n, 1)
  *
- *	The share S belongs to configuration C and item I
+ *  The share S belongs to configuration C and item I
  */
 case class ShareStatement(shareHash: String, configHash: String, item: Int) extends Statement
 
@@ -42,9 +42,9 @@ case class ShareStatement(shareHash: String, configHash: String, item: Int) exte
 /** Produced by authority 1. Each authority signs the public key. Cardinality = (1, n)
  *
  *  The public Key P belongs to configuration C and item I
- *	The shares S belong to configuration C
+ *  The shares S belong to configuration C
  *  The shares S yield the public key P
- *	The shares S each produced by the right authority
+ *  The shares S each produced by the right authority
  *  The shares S each had a valid POK
  *
  */
@@ -61,14 +61,14 @@ case class PublicKeyStatement(publicKeyHash: String, sharesHash: String, configH
  *
  *  The mix M belongs to configuration C and item I
  *  The mix/ballot P belongs to configuration C
- * 	Mixing P yields mix M
+ *  Mixing P yields mix M
  */
  case class MixStatement(mixHash: String, parentHash: String, configHash: String, item: Int, auth: Int) extends Statement
 
 
 /** Produced by each authority. Each authority signs their decryption. Cardinality = (n, 1)
  *
- *	The decryption D belongs to configuration C
+ *  The decryption D belongs to configuration C
  *  The mix M decrypts to D
  *  The mix M is the nth mix starting from the ballots, where n is the number of authorities
  */
@@ -77,9 +77,9 @@ case class DecryptionStatement(decryptionHash: String, mixHash: String, configHa
 
 /** Produced by auth 1. Each authority signs the decryption. Cardinality = (1, n)
  *
- *	The shares D belong to configuration C
+ *  The shares D belong to configuration C
  *  The shares D yield the plaintext P
- *	The shares D each produced by the right authority
+ *  The shares D each produced by the right authority
  *  The shares D each had a valid POK
  */
 case class PlaintextsStatement(plaintextsHash: String, decryptionsHash: String, configHash: String, item: Int) extends Statement
@@ -88,69 +88,69 @@ case class PlaintextsStatement(plaintextsHash: String, decryptionsHash: String, 
 /** Entry point for constructing Statments. */
 object Statement {
 
-	/** Returns the config statement for the given Config object */
-	def getConfigStatement(config: Config) = {
-		val hash = Crypto.sha512(config.asJson.noSpaces)
-		ConfigStatement(hash)
-	}
+  /** Returns the config statement for the given Config object */
+  def getConfigStatement(config: Config) = {
+    val hash = Crypto.sha512(config.asJson.noSpaces)
+    ConfigStatement(hash)
+  }
 
-	/** Returns the share statement for the given Share object, config hash and item */
-	def getShareStatement(share: Share, configHash: String, item: Int) = {
-		val shareHash = Crypto.sha512(share.asJson.noSpaces)
-		ShareStatement(shareHash, configHash, item)
-	}
+  /** Returns the share statement for the given Share object, config hash and item */
+  def getShareStatement(share: Share, configHash: String, item: Int) = {
+    val shareHash = Crypto.sha512(share.asJson.noSpaces)
+    ShareStatement(shareHash, configHash, item)
+  }
 
-	/** Returns the share statement for the given share shash, config hash and item */
-	def getShareStatement(shareHash: String, configHash: String, item: Int) = {
-		ShareStatement(shareHash, configHash, item)
-	}
+  /** Returns the share statement for the given share shash, config hash and item */
+  def getShareStatement(shareHash: String, configHash: String, item: Int) = {
+    ShareStatement(shareHash, configHash, item)
+  }
 
-	/** Returns the public key statement for the given public key String,
-		sequence of share Strings, config hash and item */
-	def getPublicKeyStatement(publicKey: String, shares: Seq[String], configHash: String, item: Int) = {
-		val publicKeyHash = Crypto.sha512(publicKey.asJson.noSpaces)
-		val sharesHash = Crypto.sha512(shares.asJson.noSpaces)
+  /** Returns the public key statement for the given public key String,
+    sequence of share Strings, config hash and item */
+  def getPublicKeyStatement(publicKey: String, shares: Seq[String], configHash: String, item: Int) = {
+    val publicKeyHash = Crypto.sha512(publicKey.asJson.noSpaces)
+    val sharesHash = Crypto.sha512(shares.asJson.noSpaces)
 
-		PublicKeyStatement(publicKeyHash, sharesHash, configHash, item)
-	}
+    PublicKeyStatement(publicKeyHash, sharesHash, configHash, item)
+  }
 
-	/** Returns the ballots statement for the ballot hash, config hash and item */
-	def getBallotsStatement(ballotHash: String, configHash: String, item: Int) = {
-		BallotsStatement(ballotHash, configHash, item)
-	}
+  /** Returns the ballots statement for the ballot hash, config hash and item */
+  def getBallotsStatement(ballotHash: String, configHash: String, item: Int) = {
+    BallotsStatement(ballotHash, configHash, item)
+  }
 
-	/** Returns the mix statement for the mix hash, parent mix hash,
-		config hash, item and authority */
-	def getMixStatement(mixHash: String, parentHash: String, configHash: String, item: Int, auth: Int) = {
-		MixStatement(mixHash, parentHash, configHash, item, auth)
-	}
+  /** Returns the mix statement for the mix hash, parent mix hash,
+    config hash, item and authority */
+  def getMixStatement(mixHash: String, parentHash: String, configHash: String, item: Int, auth: Int) = {
+    MixStatement(mixHash, parentHash, configHash, item, auth)
+  }
 
-	/** Returns the mix statement for the mix ShuffleResultDTO object,
-		parent mix hash, config hash, item and authority */
-	def getMixStatement(mix: ShuffleResultDTO, parentHash: String, configHash: String, item: Int, auth: Int) = {
-		val mixHash = Crypto.sha512(mix.asJson.noSpaces)
+  /** Returns the mix statement for the mix ShuffleResultDTO object,
+    parent mix hash, config hash, item and authority */
+  def getMixStatement(mix: ShuffleResultDTO, parentHash: String, configHash: String, item: Int, auth: Int) = {
+    val mixHash = Crypto.sha512(mix.asJson.noSpaces)
 
-		MixStatement(mixHash, parentHash, configHash, item, auth)
-	}
+    MixStatement(mixHash, parentHash, configHash, item, auth)
+  }
 
-	/** Returns the decryption statement for the decryption hash, mix hash,
-		config hash, and item */
-	def getDecryptionStatement(decryptionHash: String, mixHash: String, configHash: String, item: Int) = {
-		DecryptionStatement(decryptionHash, mixHash, configHash, item)
-	}
+  /** Returns the decryption statement for the decryption hash, mix hash,
+    config hash, and item */
+  def getDecryptionStatement(decryptionHash: String, mixHash: String, configHash: String, item: Int) = {
+    DecryptionStatement(decryptionHash, mixHash, configHash, item)
+  }
 
-	/** Returns the decryption statement for the decryption PartialDecryptionDTO
-		object, mix hash, config hash, and item */
-	def getDecryptionStatement(decryption: PartialDecryptionDTO, mixHash: String, configHash: String, item: Int) = {
-		val decryptionHash = Crypto.sha512(decryption.asJson.noSpaces)
+  /** Returns the decryption statement for the decryption PartialDecryptionDTO
+    object, mix hash, config hash, and item */
+  def getDecryptionStatement(decryption: PartialDecryptionDTO, mixHash: String, configHash: String, item: Int) = {
+    val decryptionHash = Crypto.sha512(decryption.asJson.noSpaces)
 
-		DecryptionStatement(decryptionHash, mixHash, configHash, item)
-	}
+    DecryptionStatement(decryptionHash, mixHash, configHash, item)
+  }
 
-	/** Returns the plaintexts statement for the plaintexts hash,
-		decryptions hash, config hash and item */
-	def getPlaintextsStatement(plaintextsHash: String, decryptionsHash: String, configHash: String, item: Int) = {
+  /** Returns the plaintexts statement for the plaintexts hash,
+    decryptions hash, config hash and item */
+  def getPlaintextsStatement(plaintextsHash: String, decryptionsHash: String, configHash: String, item: Int) = {
 
-		PlaintextsStatement(plaintextsHash, decryptionsHash, configHash, item)
-	}
+    PlaintextsStatement(plaintextsHash, decryptionsHash, configHash, item)
+  }
 }
